8f12bb2070a518a4c0afbe82b1842b7441bad186
Rafael <tafacreumrdsantos@gmail.com>
2025-08-25 09:04:48 -0300

    Criando calculadora imc

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,32 @@
+import '../css/global.css'
+import '../css/resultado.css'
+import TabelaImc from './TabelaImc'
+
+
+
+//destruct-(desestruturação)-acessa propriedades de outro componente
+function Resultado({resultado}){
+    return(
+        <div>
+            <div className="resultado">
+                <h2>
+                    Seu IMC é de: <span className="imcSpan">{resultado}</span>
+                </h2>
+            </div>
+
+            <table className="tabela">
+                <thead className="tabelaHeader">
+                    <tr>
+                        <th>Classificação</th>
+                        <th>IMC</th>
+                    </tr>
+                </thead>
+                <tbody className="tabelaBody">
+                    <tr>{TabelaImc(resultado)}</tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Resultado
